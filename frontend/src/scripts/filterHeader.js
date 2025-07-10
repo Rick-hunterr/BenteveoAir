aea6c23671de7ae9159915d1b0ac4c185bc3a6d9
tiziano <tizianoharo09@gmail.com>
2025-07-09 21:00:49 -0300

    feat: agregado filtro de paquetes turísticos por tipo

--- frontend/src/scripts/filterHeader.js
+++ frontend/src/scripts/filterHeader.js
@@ -1,1 +1,32 @@
-// Por ahora vacio, no molesten :D
+const allFilterBtn = document.getElementById("all-btn");
+const natFilterBtn = document.getElementById("nat-btn");
+const interFilterBtn = document.getElementById("inter-btn");
+
+packages.forEach((package) => {
+  const typePackage = package.getAttribute("data-type");
+
+  allFilterBtn.addEventListener("click", () => {
+    package.classList.remove("hidden");
+    package.classList.add("flex");
+  });
+
+  natFilterBtn.addEventListener("click", () => {
+    if (typePackage == "international") {
+      package.classList.remove("flex");
+      package.classList.add("hidden");
+    } else {
+      package.classList.remove("hidden");
+      package.classList.add("flex");
+    }
+  });
+
+  interFilterBtn.addEventListener("click", () => {
+    if (typePackage == "national") {
+      package.classList.remove("flex");
+      package.classList.add("hidden");
+    } else {
+      package.classList.remove("hidden");
+      package.classList.add("flex");
+    }
+  });
+});
