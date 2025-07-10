0230341f5baf0b5454705f847912d6d00f03f424
tiziano <tizianoharo09@gmail.com>
2025-07-09 22:30:42 -0300

    refactor: mejora en la logica del filtrado de paquetes

--- frontend/src/scripts/filterHeader.js
+++ frontend/src/scripts/filterHeader.js
@@ -2,31 +2,28 @@ const allFilterBtn = document.getElementById("all-btn");
 const natFilterBtn = document.getElementById("nat-btn");
 const interFilterBtn = document.getElementById("inter-btn");
 
-packages.forEach((package) => {
-  const typePackage = package.getAttribute("data-type");
+const filterPackagesByType = (filterType) => {
+  packages.forEach((package) => {
+    const typePackage = package.getAttribute("data-type");
 
-  allFilterBtn.addEventListener("click", () => {
-    package.classList.remove("hidden");
-    package.classList.add("flex");
-  });
-
-  natFilterBtn.addEventListener("click", () => {
-    if (typePackage == "international") {
-      package.classList.remove("flex");
-      package.classList.add("hidden");
-    } else {
+    if (filterType === "all" || typePackage === filterType) {
       package.classList.remove("hidden");
       package.classList.add("flex");
-    }
-  });
-
-  interFilterBtn.addEventListener("click", () => {
-    if (typePackage == "national") {
+    } else {
       package.classList.remove("flex");
       package.classList.add("hidden");
-    } else {
-      package.classList.remove("hidden");
-      package.classList.add("flex");
     }
   });
+};
+
+allFilterBtn.addEventListener("click", () => {
+  filterPackagesByType("all");
+});
+
+natFilterBtn.addEventListener("click", () => {
+  filterPackagesByType("national");
+});
+
+interFilterBtn.addEventListener("click", () => {
+  filterPackagesByType("international");
 });
