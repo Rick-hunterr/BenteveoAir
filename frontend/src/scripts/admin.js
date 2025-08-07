5beab768e5cbf0df7743a49a2fdd4b0c3511b1ed
Dunkan1920 <dunkan1920@gmail.com>
2025-08-07 03:40:11 -0300

    Fix: El botón de perfil de admin ya no paraliza la página al hacerle click

--- frontend/src/scripts/admin.js
+++ frontend/src/scripts/admin.js
@@ -14,12 +14,14 @@ if (userMenu_Btn && modalsContainer) {
   userMenu_Btn.addEventListener("click", (e) => {
     e.stopPropagation();
     modalsContainer.classList.remove("hidden");
+    perfilModal.classList.remove("hidden");
     perfilModal.showModal();
   });
 
   window.addEventListener("click", (e) => {
     if (!userMenu_Btn.contains(e.target) && !modalsContainer.contains(e.target)){
       perfilModal.close();
+      perfilModal.classList.add("hidden");
       modalsContainer.classList.add("hidden");
     }
   });
@@ -27,48 +29,55 @@ if (userMenu_Btn && modalsContainer) {
 
 closePerfil.addEventListener("click", () => {
   perfilModal.close();
+  perfilModal.classList.add("hidden");
   modalsContainer.classList.add("hidden");
 });
-
-
-
-//=========================================================================
-//============================== CERRAR SESIÓN ==========================
-//=========================================================================
-
-
-// if (logoutBtn) {
-//   logoutBtn.addEventListener("click", () => {
-//     localStorage.removeItem("token");
-//     alert("Sesión cerrada");
-//     location.reload();
-//   });
-// }
-
-
 
 // //============================================================================
 // //========================= CAMBIAR DATOS MODAL ============================
 // //============================================================================
 const cambiarDatosAdmin_Btn = document.getElementById("cambiarDatosAdmin_Btn");
 const datosAdmin_Modal = document.getElementById("datosAdmin_Modal");
+const guardarDatosAdmin_Btn = document.getElementById("guardarDatosAdmin_Btn");
 const cerrarDatosAdmin = document.getElementById("cerrarDatosAdmin");
 
 cambiarDatosAdmin_Btn.addEventListener("click", (e) => {
   e.preventDefault();
-  // perfilModal.close();
+  perfilModal.close();
+  perfilModal.classList.add("hidden");
 
+  datosAdmin_Modal.classList.remove("hidden");
   datosAdmin_Modal.showModal();
 });
 
 cerrarDatosAdmin.addEventListener("click", () => {
   datosAdmin_Modal.close();
+  datosAdmin_Modal.classList.add("hidden");
   modalsContainer.classList.add("hidden");
 });
 
-// modalsContainer.addEventListener("click", (e) => {
-//   if (e.target === modalsContainer) {
-//     datosAdmin_Modal.close();
-//     modalsContainer.classList.add("hidden");
-//   }
-// });
+guardarDatosAdmin_Btn.addEventListener("click", () => {
+  datosAdmin_Modal.close();
+  datosAdmin_Modal.classList.add("hidden");
+  modalsContainer.classList.add("hidden");
+});
+
+//=========================================================================
+//============================== CERRAR SESIÓN ==========================
+//=========================================================================
+const logout_Btn = document.getElementById("logout_Btn");
+
+if (logout_Btn) {
+  logout_Btn.addEventListener("click", () => {
+    perfilModal.close();
+    perfilModal.classList.add("hidden");
+    modalsContainer.classList.add("hidden");
+    
+    localStorage.removeItem("token");
+    alert("Sesión cerrada");
+    location.reload();
+  });
+}
+
+
+
