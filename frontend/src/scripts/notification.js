bd3377aa50d16e05a80c7c14aa9fa3ac847506c8
tiziano <tizianoharo09@gmail.com>
2025-08-12 19:08:15 -0300

    fix: arreglar notificacion al momento de confirmar un paquete

--- frontend/src/scripts/notification.js
+++ frontend/src/scripts/notification.js
@@ -1,13 +1,13 @@
 const notification = document.getElementById("notification");
-const addToCartBtns = document.querySelectorAll(".add-to-cart");
+const modalPackage = document.getElementById("package-modal");
 let counter = JSON.parse(localStorage.getItem("packages") || "[]").length;
 
-if (addToCartBtns.length > 0) {
-  addToCartBtns.forEach((addToCartBtn) => {
-    addToCartBtn.addEventListener("click", () => {
-      counter++;
-      notification.textContent = counter;
-    });
+if (modalPackage) {
+  const confirmPkgBtn = modalPackage.querySelector("#confirm-package");
+
+  confirmPkgBtn.addEventListener("click", () => {
+    counter++;
+    notification.textContent = counter;
   });
 }
 
