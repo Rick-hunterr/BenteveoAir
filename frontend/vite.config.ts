2bf674e36744fadf7aca2922c0743e6f3ace7214
Pablo Daniel Santellan <pablolodanielsantellan@gmail.com>
2025-08-14 14:22:55 -0300

    Refactor: mejorar la configuración de Vite para manejo en local y produccion

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,26 +2,35 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 // Carga la variable de entorno VITE_API_URL
-//Por si uno lee esto lo que hice fue crear una env para no tener que estar cambiando la URL del backend
-//cada vez que quiero probar algo en local o en produccion
-//Si no existe, usa http://localhost:10000 como valor por defecto (la URL del backend en local)
+// Por si uno lee esto: se usa para no tener que cambiar la URL del backend
+// cada vez que probás en local o producción.
+// Si no existe, usa http://localhost:10000 como valor por defecto (backend local)
+
+// URL del backend en producción (Render) o local
 const API_URL = process.env.VITE_API_URL || "http://localhost:10000";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  // Configuración para desarrollo local
   server: {
     port: 5173,
     proxy: {
-      // esto es ssolo para desarrollo local
       "/api": {
         target: API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-    allowedHosts: ["033fcfca19db.ngrok-free.app"], //Temporal
+    //allowedHosts: ["033fcfca19db.ngrok-free.app"], // temporal
+    allowedHosts: ["0.0.0.0"], // permite acceder desde cualquier host local o túnel
   },
   define: {
     "import.meta.env.VITE_API_URL": JSON.stringify(API_URL),
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html",
+    },
   },
 });
