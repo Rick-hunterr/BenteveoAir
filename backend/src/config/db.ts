c978fe951f37d2a421812c0a3b3bd0328bed8798
Pablo Daniel Santellan <pablolodanielsantellan@gmail.com>
2025-08-14 11:45:38 -0300

    Refactor: ajustar la configuración de la base de datos para entornos de producción y desarrollo en render

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -10,22 +10,43 @@ import * as dotenv from "dotenv"
 
 dotenv.config()
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  synchronize: true,
-  logging: false,
-  entities: [
-    Usuario,
-    Ubicacion,
-    Producto,
-    Servicio,
-    DestinoVuelo,
-    Orden,
-    DetalleOrden,
-  ],
-})
+const isProduction = process.env.NODE_ENV === "production"
+
+export const AppDataSource = new DataSource(
+  isProduction
+    ? {
+        type: "postgres",
+        url: process.env.DATABASE_URL, // Render usa esto
+        ssl: { rejectUnauthorized: false },// Render requiere SSL
+        synchronize: true, //En producción, esto debería ser false
+        logging: false,
+        entities: [
+          Usuario,
+          Ubicacion,
+          Producto,
+          Servicio,
+          DestinoVuelo,
+          Orden,
+          DetalleOrden,
+        ],
+      }
+    : {
+        type: "postgres",
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || "5432"),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        logging: false,
+        entities: [
+          Usuario,
+          Ubicacion,
+          Producto,
+          Servicio,
+          DestinoVuelo,
+          Orden,
+          DetalleOrden,
+        ],
+      }
+)
--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -2,11 +2,13 @@ import "reflect-metadata"
 import { AppDataSource } from "../config/db"
 import app from "./app"
 
+const PORT = process.env.PORT || 3000
+
 AppDataSource.initialize()
   .then(() => {
     console.log("Base de datos conectada")
-    app.listen(3000, () => {
-      console.log("Servidor corriendo en http://localhost:3000")
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`)
     })
   })
   .catch((err) => console.error(err))
